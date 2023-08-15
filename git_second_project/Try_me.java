0e71ed32cc7028d7170cb466cd24900fdc632a80
Ahmad Farhad <farhad.zammani@gmail.com>
2023-08-15 09:59:28 -0700

    A bit changes

--- git_second_project/Try_me.java
+++ git_second_project/Try_me.java
@@ -2,6 +2,6 @@ package git_second_project;
 
 public class Try_me {
 public static void main(String[] args) {
-	System.out.println("everythig will be alright");
+	System.out.println("everythig will be alright and push this in mimoBrach");
 }
 }
